3426488273366390ea792d12c3e5ec7252633cbe
priz80 <priz8090@gmail.com>
2026-01-29 00:17:51 +0300

    Delete underlining.js

--- js/underlining.js
+++
@@ -1,8 +0,0 @@
-const items = document.querySelectorAll('.item-menu');
-
-items.forEach((item) => {
-    item.addEventListener('click', () => {
-        items.forEach(el => el.classList.remove('active'));
-        item.classList.add('active');
-    });
-});
